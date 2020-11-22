a009fb0ea5e7926d1ca41bf8e83bccf04684918e
Anton Kablukov <antoha7@gmail.com>
2020-11-22 22:24:51 +0100

    refactor: move adding to db logic to db folder

--- product-service/db/addProductToDB.ts
+++ product-service/db/addProductToDB.ts
@@ -1,23 +1,23 @@
 interface Values {
     title: string;
     description: string;
-    price: number;
+    price: string | number;
     image: string;
-    count: number;
+    count: string | number;
 }
 
 export const addProductToDB = async (client, values: Values) => {
     try {
         const { title, description, price, image, count } = values;
-        console.log({title});
+
         await client.query('BEGIN');
         const queryTextProducts = 'INSERT into products (title, description, price, image) VALUES ($1, $2, $3, $4) RETURNING id';
-        const queryValuesProducts = [title, description, price, image];
+        const queryValuesProducts = [title, description, Number(price), image];
         const responseProducts = await client.query(queryTextProducts, queryValuesProducts)
     
         const queryTextStocks = 'INSERT into stocks (count, product_id) VALUES ($1, $2)';
         const productId = responseProducts.rows[0].id;
-        const queryValuesStocks = [count, productId];
+        const queryValuesStocks = [Number(count), productId];
         await client.query(queryTextStocks, queryValuesStocks);
     
         await client.query('COMMIT');
--- product-service/handlers/catalogBatchProcess/catalogBatchProcess.ts
+++ product-service/handlers/catalogBatchProcess/catalogBatchProcess.ts
@@ -7,52 +7,39 @@ import cors from '@middy/http-cors';
 import httpSecurityHeaders from '@middy/http-security-headers';
 import sqsPartialBatchFailure from'@middy/sqs-partial-batch-failure';
 import 'source-map-support/register';
-import { dbOptions } from '../../db';
+import { dbOptions, addProductToDB } from '../../db';
 import { Client } from 'pg';
 
 const catalogBatchProcess = middy(async (event: SQSEvent) => {
-  const client = new Client(dbOptions);
-  await client.connect();
   
-  try {
     const processMessage = async (record: SQSRecord) => {
       const productData = JSON.parse(record.body)
-      const { title, description, price, image, count } = productData;
-
-      await client.query('BEGIN');
-        const queryTextProducts = 'INSERT into products (title, description, price, image) VALUES ($1, $2, $3, $4) RETURNING id';
-        const queryValuesProducts = [title, description, Number(price), image];
-        const responseProducts = await client.query(queryTextProducts, queryValuesProducts)
-    
-        const queryTextStocks = 'INSERT into stocks (count, product_id) VALUES ($1, $2)';
-        const productId = responseProducts.rows[0].id;
-        const queryValuesStocks = [Number(count), productId];
-        await client.query(queryTextStocks, queryValuesStocks);
-    
-        await client.query('COMMIT');
+      const client = new Client(dbOptions);
+      await client.connect();
+      
+      try {
+        const productId = await addProductToDB(client, productData);
 
         const { rows: product } = await client.query(
-        `SELECT p.*, s.count FROM products p LEFT JOIN stocks s ON p.id = s.product_id WHERE p.id = $1`,
-      [productId]);
-      console.log('product id: ', productId);
+          `SELECT p.*, s.count FROM products p LEFT JOIN stocks s ON p.id = s.product_id WHERE p.id = $1`,
+        [productId]);
 
-      if (!product) {
-        throw new createError.BadRequest('Product creation failed');
+        if (!product) {
+          throw new createError.BadRequest('Product creation failed');
+        }
+
+        return;
+      } catch (error) {
+        console.log(error);
+        throw new createError.InternalServerError();
+      } finally {
+        client.end();
       }
-
-      return;
     }
-  
+
     const messageProcessingPromises = event.Records.map(processMessage);
 
     return Promise.allSettled(messageProcessingPromises)
-   
-  } catch (error) {
-    console.log(error);
-    throw new createError.InternalServerError();
-  } finally {
-    client.end();
-  }
 })
 
 catalogBatchProcess
